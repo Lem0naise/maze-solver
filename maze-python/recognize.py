31b94dcc708ff2d63788ca7e17f7cbb67a9ea7c2
Zac Nolan <zac1i1nolan@gmail.com>
2023-01-18 18:54:56 +0000

    redid NB

--- maze-python/recognize.py
+++ maze-python/recognize.py
@@ -1,33 +1,33 @@
 import cv2
 
+# NB
+# opencv2 uses BGR colours, not RGB
+# coordinates are in (y, x) format not (x, y)
+
 cap = cv2.VideoCapture(0)
 
 cap.set(3, 16)
 cap.set(4, 9)
 
-number = 20000
+number = 40000
+
 while(True):
       
     ret, frame = cap.read()
-
     height, width = frame.shape[:2]
 
-    # (b, g, r) for some reason, and not (r, g, b)
-    # this gets the pixel rgb values at coords (72, 34)
-    (b, g, r) = frame[34, 72] 
 
     # my first attempt at making all 'white' pixels red
     for y in range(0, height, 3):
         for x in range(0, width, 3):
             (b, g, r) = frame[y, x] # (b,g,r)
             
-            if r+g > 0:
-                if (float(r) + float(g) + float(b) > 140*3) and ((float(r**2 + g**2 + b**2)/3.0) - (float(r + g + b)/3.0)**2) < number:
+            if (float(r) + float(g) + float(b) > 140*3) and ((float(r**2 + g**2 + b**2)/3.0) - (float(r + g + b)/3.0)**2) < number:
 
-                    for i in range(y-1, y+1):
-                        for j in range(x-1, x+1):
-                            frame[i, j] = (0, 0, 255)
-            
+                for i in range(y-1, y+1):
+                    for j in range(x-1, x+1):
+                        frame[i, j] = (0, 0, 255)
+        
     
     # this is how you draw markers on the frame
     #cv2.drawMarker(frame, (width//2, height//2), color=(0, 0, 255), markerType=cv2.MARKER_CROSS, thickness=3, markerSize=50)
