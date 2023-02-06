938d1db6ed76fcfd96f350b292e237fb791d9b87
Zac Nolan <zac1i1nolan@gmail.com>
2023-02-06 20:29:36 +0000

    MAJOR CLEAN UP

--- maze-python/main.py
+++ maze-python/main.py
@@ -1,18 +1,17 @@
 from solve import MazeSolver;
 from recognize import Recogniser;
 from time import sleep;
-import json
-import webbrowser
 
 # Runtime Parameters (N.B. Makes significantly slower)
 show_debug = False
 show_loading = False
 
-# Solution Display Options (Does not affect speed as much)
-line_thickness = 4
+# Solution Display Options (Negligibly affect speed)
+line_thickness = 6
 line_colour = (222, 98, 91)
-entity = True # idk what to call this
-delay = 50 # ms, time between path frames, when entity = False set to 1, when entity = True set to 50 or otherwise
+entity_colour = (50, 50, 50) # colour of snake head
+entity = True # boolean, snake head or not
+delay = 1 # ms, integer, >0
 
 
 # TODO CANNOT GO BELOW 
@@ -26,19 +25,4 @@ maze = frame;
 height = len(maze)
 width = len(maze[0])
 
-solver = MazeSolver(maze, recogniser, show_debug, show_loading, line_thickness, line_colour, entity, delay);
-
-
-# sending maze & path to server
-def write_json():
-    height, width = len(maze), len(maze[0]);
-    write_dic = {
-        "path": solver.path,
-        "hw": [height, width],
-        "maze":maze,
-    }
-    # open link in web browser
-    pw = "password"
-    webbrowser.open('http://localhost:3000?password=' + pw + '&maze='+ json.dumps(write_dic))
-
-#write_json();
+solver = MazeSolver(maze, recogniser, show_debug, show_loading, line_thickness, line_colour, entity, entity_colour, delay);
--- maze-python/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-gniser = Recogniser();
-print('finished with recogniser')
-frame = recogniser.frame;
-while frame.any() == None:
-    frame = recogniser.frame;
-maze = frame;
-
-height = len(maze)
-width = len(maze[0])
